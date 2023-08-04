b9c3a69ac8b386276b36632ef6e1c4cd4055e24b
d-o0o-b11 <jimin8830@naver.com>
2023-08-04 16:30:05 +0900

    ✨ - user_tap 주석 제거

--- src/user_tap/dto/create-user-tap-text.dto.ts
+++ src/user_tap/dto/create-user-tap-text.dto.ts
@@ -1,25 +1,18 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class CreateUserTapTextDto {
-  // @IsString()
-  // @ApiProperty({
-  //   description: '탭 타입[텍스트]',
-  //   example: 'text',
-  // })
-  // tap_type: string;
-
   @IsString()
   @ApiProperty({
-    description: '제목',
-    example: '텍스트 제목제목',
+    description: "제목",
+    example: "텍스트 제목제목",
   })
   title: string;
 
   @IsString()
   @ApiProperty({
-    description: '내용',
-    example: '텍스트 내용내용',
+    description: "내용",
+    example: "텍스트 내용내용",
   })
   context: string;
 }
--- src/user_tap/dto/update-user-tap-text.dto.ts
+++ src/user_tap/dto/update-user-tap-text.dto.ts
@@ -1,34 +1,26 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsBoolean, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class UpdateUserTapTextDto {
   @IsNumber()
   @ApiProperty({
-    description: '수정 tap id',
+    description: "수정 tap id",
     example: 1,
   })
   tap_id: number;
 
-  // @IsOptional()
-  // @IsString()
-  // @ApiPropertyOptional({
-  //   description: '텍스트 제목',
-  //   example: '텍스트 수정제목',
-  // })
-  // title?: string;
-
   @IsOptional()
   @IsString()
   @ApiPropertyOptional({
-    description: '수정 내용',
-    example: '텍스트 수정내용',
+    description: "수정 내용",
+    example: "텍스트 수정내용",
   })
   context?: string;
 
   @IsBoolean()
   @IsOptional()
   @ApiPropertyOptional({
-    description: '공개 true, 비공개 false',
+    description: "공개 true, 비공개 false",
     example: true,
   })
   toggle_state?: boolean;
@@ -36,7 +28,7 @@ export class UpdateUserTapTextDto {
   @IsBoolean()
   @IsOptional()
   @ApiPropertyOptional({
-    description: '펼친 상태 true, 접은 상태 false',
+    description: "펼친 상태 true, 접은 상태 false",
     example: true,
   })
   folded_state?: boolean;
